e88cc0502dad6fca1bc18808fdedf6f5681198f5
John Zhang <john.jiabin.zhang@gmail.com>
2016-04-05 17:50:23 +1000

    HAIL generator code.

--- rpython/translator/mu/hail.py
+++ rpython/translator/mu/hail.py
@@ -2,3 +2,76 @@
 Generate the Heap Allocation and Initialisation Language (HAIL) script
 to initialise the global cells.
 """
+from rpython.mutyper.muts import mutype
+
+
+class _HAILName:
+    _name_dic = {}
+
+    def __init__(self, name):
+        if name in _HAILName._name_dic:
+            n = _HAILName._name_dic[name] + 1
+        else:
+            n = 0
+        self.name = "%s_%d" % (name, n)
+
+    def __str__(self):
+        return "$%s" % self.name
+
+
+class HAILGenerator:
+    def __init__(self):
+        self.gcells = []
+        self._refs = {}
+
+    def add_gcell(self, gcell):
+        self.gcells.append(gcell)
+
+        self._find_refs(gcell._obj, gcell)
+
+    def _find_refs(self, obj, gcell=None):
+        if isinstance(obj, (mutype._muref, mutype._muiref)):
+            if obj not in self._refs:
+                self._refs[obj] = gcell.mu_name if gcell else _HAILName(mutype.mu_typeOf(obj).mu_name._name)
+            elif isinstance(self._refs[obj], _HAILName) and gcell:
+                self._refs[obj] = gcell.mu_name
+            else:
+                return
+            self._find_refs(obj._obj0)
+
+        elif isinstance(obj, (mutype._mustruct, mutype._muhybrid)):
+            for fld in mutype.mu_typeOf(obj)._flds:
+                self._find_refs(obj._getattr(fld))
+
+        elif isinstance(obj, (mutype._mumemarray)):
+            if isinstance(obj._OF, (mutype.MuContainerType, mutype.MuRef, mutype.MuIRef)):
+                for itm in obj:
+                    self._find_refs(itm)
+
+    def codegen(self, fp):
+        # Allocate everything first
+        for r, n in self._refs.items():
+            obj_t = mutype.mu_typeOf(r._obj0)
+            if isinstance(obj_t, mutype.MuHybrid):
+                fp.write(".newhybrid %s <%s> %d\n" % (n, obj_t.mu_name, len(r._obj0._getattr(obj_t._varfld))))
+            else:
+                fp.write(".new %s <%s>\n" % (n, obj_t.mu_name))
+
+        for r, n in self._refs.items():
+            fp.write(".init %s = %s\n" % (n, self._getinitstr(r._obj0)))
+
+    def _getinitstr(self, obj):
+        if isinstance(obj, (mutype._muprimitive, mutype._munullref)):
+            return str(obj)
+
+        elif isinstance(obj, (mutype._mustruct, mutype._muhybrid)):
+            return "{%s}" % ' '.join([self._getinitstr(obj._getattr(fld)) for fld in mutype.mu_typeOf(obj)._names])
+
+        elif isinstance(obj, mutype._mumemarray):
+            return "{%s}" % ' '.join([self._getinitstr(itm) for itm in obj])
+
+        elif isinstance(obj, (mutype._muref, mutype._muiref)):
+            assert obj in self._refs
+            return str(self._refs[obj])
+        else:
+            raise TypeError("Unknown value '%s' of type '%s'." % (obj, mutype.mu_typeOf(obj)))
---
+++ rpython/translator/mu/test/test_hail.py
@@ -0,0 +1,102 @@
+from rpython.mutyper.mutyper import MuTyper
+from rpython.mutyper.tools.textgraph import print_graph
+from rpython.rtyper.test.test_llinterp import gengraph
+from rpython.translator.mu.preps import prepare
+from ..hail import _HAILName, HAILGenerator
+from rpython.mutyper.muts.mutype import *
+from rpython.mutyper.muts.muentity import *
+from StringIO import StringIO
+from rpython.rtyper.lltypesystem import lltype as ll
+from rpython.rtyper.lltypesystem.rstr import STR
+from rpython.mutyper.ll2mu import ll2mu_val, ll2mu_ty
+
+
+def test_linkedlist():
+    Node = MuStruct('Node')
+    Node._setfields([('data', int64_t), ('nxt', MuRef(Node))])
+
+    nd1 = new(Node)
+    nd2 = new(Node)
+    gcell1 = MuGlobalCell(mu_typeOf(nd1))
+    gcell2 = MuGlobalCell(mu_typeOf(nd2))
+
+    gcell1._obj = nd1
+    gcell2._obj = nd2
+
+    iref_nd1 = nd1._getiref()
+    iref_nd2 = nd2._getiref()
+    iref_nd1.data._obj = int64_t(1)
+    iref_nd1.nxt._obj = nd2
+    iref_nd2.data._obj = int64_t(2)
+    iref_nd2.nxt._obj = nd1
+
+
+
+    hailgen = HAILGenerator()
+    hailgen.add_gcell(gcell1)
+    assert len(hailgen._refs) == 2
+    assert isinstance(hailgen._refs[nd2], _HAILName)
+
+    hailgen.add_gcell(gcell2)
+    assert len(hailgen._refs) == 2
+    assert hailgen._refs[nd2] == gcell2.mu_name
+
+    strio = StringIO()
+    hailgen.codegen(strio)
+    out = strio.getvalue()
+    strio.close()
+
+    assert '.new @gclrefsttNode_0 <@sttNode>' in out
+    assert '.new @gclrefsttNode_1 <@sttNode>' in out
+    assert '.init @gclrefsttNode_0 = {1 @gclrefsttNode_1}' in out
+    assert '.init @gclrefsttNode_1 = {2 @gclrefsttNode_0}' in out
+
+
+def test_string():
+    string = "hello"
+    ll_ps = ll.malloc(STR, len(string))
+    ll_ps.hash = hash(string)
+    for i in range(len(string)):
+        ll_ps.chars[i] = string[i]
+
+    mu_t = ll2mu_ty(ll_ps._TYPE)
+    mu_rs = ll2mu_val(ll_ps)
+
+    gcell = MuGlobalCell(mu_t)
+    gcell._obj = mu_rs
+
+    hailgen = HAILGenerator()
+    hailgen.add_gcell(gcell)
+    strio = StringIO()
+    hailgen.codegen(strio)
+    out = strio.getvalue()
+    strio.close()
+
+    alloc_str = '.newhybrid %s <%s> 5' % (gcell.mu_name, mu_t.TO.mu_name)
+    assert alloc_str in out
+    init_str = '.init %s = {%d 5 {%s}}' % (gcell.mu_name, ll_ps.hash, ' '.join(map(lambda c: str(ord(c)), ll_ps.chars)))
+    assert init_str in out
+
+
+def test_excobj():
+    def main(argv):
+        return int(argv[0]) * 10
+
+    t, _, g = gengraph(main, [[str]], backendopt=True)
+
+    t.graphs = prepare(t.graphs, g)
+
+    mutyper = MuTyper()
+    for _g in t.graphs:
+        mutyper.specialise(_g)
+
+    hailgen = HAILGenerator()
+    for gcell, ldgcell in mutyper.ldgcells.items():
+        print gcell, ldgcell
+        hailgen.add_gcell(gcell)
+
+    strio = StringIO()
+    hailgen.codegen(strio)
+    out = strio.getvalue()
+    strio.close()
+    print out
