967d8e5f5393f7c7d7429ec0221a6094750c7c5d
John Zhang <john.jiabin.zhang@gmail.com>
2016-05-31 17:44:12 +1000

    Added a few more functions.

--- rpython/translator/mu/rpyc/src/utils.c
+++ rpython/translator/mu/rpyc/src/utils.c
@@ -8,21 +8,14 @@
 
 #define _PYPY_LINUX_PREFIX(fn) __pypy_mu_linux_ ## fn
 
-int _PYPY_LINUX_PREFIX(stat64)(const char* path, struct stat *buf) {
-  return stat(path, buf);
-}
+int _PYPY_LINUX_PREFIX(stat64)(const char* path, struct stat *buf) { return stat(path, buf); }
+int _PYPY_LINUX_PREFIX(fstat64)(int fd, struct stat *buf) { return fstat(fd, buf); }
+int _PYPY_LINUX_PREFIX(lstat64)(const char *path, struct stat *buf) { return lstat(path, buf); }
+int _PYPY_LINUX_PREFIX(mknod)(const char* pathname, mode_t mode, dev_t dev) { return mknod(pathname, mode, dev); }
 
-int _PYPY_LINUX_PREFIX(fstat64)(int fd, struct stat *buf) {
-  return fstat(fd, buf);
-}
+extern char** environ;
+char** _PYPY_LINUX_PREFIX(get_environ)() { return environ; }
 
-int _PYPY_LINUX_PREFIX(lstat64)(const char *path, struct stat *buf) {
-  return lstat(path, buf);
-}
-
-int _PYPY_LINUX_PREFIX(mknod)(const char* pathname, mode_t mode, dev_t dev) {
-  return mknod(pathname, mode, dev);
-}
 #elif defined(__APPLE__)
 #endif
 
@@ -41,4 +34,12 @@ _PYPY_WAITPID_MACRO(WIFSIGNALED)
 _PYPY_WAITPID_MACRO(WIFSTOPPED)
 _PYPY_WAITPID_MACRO(WSTOPSIG)
 _PYPY_WAITPID_MACRO(WTERMSIG)
+
+
+#ifndef _BSD_SOURCE
+#define _BSD_SOURCE
+#endif
+dev_t _PYPY_MACRO_PREFIX(makedev)(int maj, int min) { return makedev(maj, min); }
+unsigned int _PYPY_MACRO_PREFIX(major)(dev_t dev) { return major(dev); }
+unsigned int _PYPY_MACRO_PREFIX(minor)(dev_t dev) { return minor(dev); }
 
