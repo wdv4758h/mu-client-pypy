e73fa3cb390d61f61d152e7ca8b9e4fd62546b4c
John Zhang <john.jiabin.zhang@gmail.com>
2016-05-31 11:29:01 +1000

    rename and changed the macro that distinguishes the OS platform

--- rpython/translator/mu/rpyc/src/utils.c
+++ rpython/translator/mu/rpyc/src/utils.c
@@ -1,9 +1,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-#if defined(_POSIX_C_SOURCE)
+#if defined(__linux__)
 int stat64(const char* path, struct stat *buf) {
   return stat(path, buf);
 }
+#elif defined(__APPLE__)
 #endif
