60658db8c4eff364e766fdd16622ebbc04dccb7a
John Zhang <john.zhang@anu.edu.au>
2016-09-26 21:32:50 +1000

    Struct definition

--- rpython/translator/mu/mutype.py
+++ rpython/translator/mu/mutype.py
@@ -1,10 +1,6 @@
 """
 Define Mu Type System in similar fashion of Low Level Type System.
 """
-import weakref
-from types import NoneType
-import math
-from rpython.rlib.objectmodel import Symbolic
 from rpython.rlib import rarithmetic
 from rpython.rtyper.lltypesystem import lltype, rffi
 
@@ -43,6 +39,7 @@ class MuNumber(MuPrimitive, lltype.Number):
 
     def get_value_type(self):
         return self._val_t
+
 
 class MuIntType(MuNumber):
     def __init__(self, name, value_type_cls):
@@ -83,3 +80,76 @@ MU_FLOAT = MuFloatType("MU_FLOAT", rffi.r_singlefloat, 32)
 MU_DOUBLE = MuFloatType("MU_DOUBLE", rarithmetic.r_longfloat, 64)
 
 MU_VOID = MuPrimitive("MU_VOID", None)
+
+
+# ----------------------------------------------------------
+# Container types
+class MuContainerType(MuType, lltype.ContainerType):
+    _gckind = None  # all
+
+
+class MuForwardReference(MuContainerType, lltype.ForwardReference):
+    def become(self, realcontainertype):
+        if not isinstance(realcontainertype, MuContainerType):
+            raise TypeError("MuForwardReference can only be to a MuContainer, "
+                            "not %r" % (realcontainertype,))
+        lltype.ForwardReference.become(self, realcontainertype)
+
+
+class MuStruct(MuContainerType):
+    # methods that are consistent with lltype.Struct
+    _consistent_methods = [
+        '_first_struct',
+        '__getattr__',
+        '_nofield',
+        '_str_fields',
+        '__str__',
+        '_short_name',
+        '_immutable_field',
+    ]
+
+    def __init__(self, name, *fields, **kwds):
+        self._name = self.__name__ = name
+        flds = {}
+        names = []
+        self._arrayfld = None
+        for name, typ in fields:
+            if name.startswith('_'):
+                raise NameError("%s: field name %r should not start with "
+                                  "an underscore" % (self._name, name,))
+            if typ is MU_VOID:
+                raise TypeError("%s: void field '%s" % (self._name, name))
+
+            names.append(name)
+            if name in flds:
+                raise TypeError("%s: repeated field name" % self._name)
+            flds[name] = typ
+
+        self._flds = lltype.frozendict(flds)
+        self._names = tuple(names)
+
+        self._install_extras(**kwds)
+
+    def _is_atomic(self):
+        # Mu memory order does not depend on data structure
+        raise NotImplementedError
+
+    def _is_varsize(self):
+        return False    # struct in Mu is always fixed size
+
+    def __getitem__(self, idx):     # support indexing into a struct
+        return self._flds[self._names[idx]]
+
+    def _allocate(self, initialization='raw', parent=None, parentindex=None):
+        # return _mustruct(self, initialization=initialization,
+        #                parent=parent, parentindex=parentindex)
+        raise NotImplementedError
+
+    def _container_example(self):
+        # return _mustruct(self)
+        raise NotImplementedError
+
+for mtd in MuStruct._consistent_methods:
+    setattr(MuStruct, mtd, lltype.Struct.__dict__[mtd])
+
+# TODO: write tests
