46f2e64f560352834664ac9a874e7206f1f9b815
John Zhang <john.zhang@anu.edu.au>
2016-11-01 18:12:18 +1100

    update generation script

--- rpython/translator/mu/test_impl/gen_csrcs.sh
+++ rpython/translator/mu/test_impl/gen_csrcs.sh
@@ -4,3 +4,4 @@ python test_fib.py --impl fast --testjit -o test_fib.c
 python test_multifunc.py --impl fast --testjit -o test_multifunc.c
 python test_constfunc.py --impl fast --testjit -o test_constfunc.c
 python test_ccall.py --impl fast --testjit -o test_ccall.c
+python test_milsum.py --impl fast --testjit -o test_milsum.c
