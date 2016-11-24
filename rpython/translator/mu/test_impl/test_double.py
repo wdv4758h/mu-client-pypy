472f171164e2d3de3e12f323825a021d101c3bbc
John Zhang <john.zhang@anu.edu.au>
2016-11-24 13:34:52 +1100

    add double and int64 conversion tests

--- rpython/translator/mu/test_impl/test_double.py
+++ rpython/translator/mu/test_impl/test_double.py
@@ -547,5 +547,271 @@ def test_double_arg_pass(cmdopt):
             "test_fnc_sig": sig_dbldbl_dbl,
             "result_type": dbl,
             "handle_conv_dst_type": 'double',
+            "@dbl": dbl,
+        }
+
+    def extend_with_entrypoint(bldr, id_dict, rmu):
+        """
+            Extend the bundle with:
+                .typedef @i32 = int<32>
+                .const @pi <@dbl> = 3.1415926
+                .const @e <@dbl> = 2.71828
+                .typedef @i8 = int<8>
+                .typedef @pi8 = uptr<@i8>
+                .typedef @ppi8 = uptr<@pi8>
+                .global @result <@i8>
+                .funcsig @sig_i8ppi8_ = (@i8 @ppi8) -> ()
+                .funcdef @entry VERSION @entry_v1 <@sig_i8ppi8_> {
+                    .blk0 (<@i8> argc <@ppi8> argv):
+                        %res = CALL @test_fnc (@pi @e)
+                        STORE <i8> @result %res
+                        COMMINST @uvm.thread_exit
+                }
+            """
+        if '@i8' in id_dict:
+            i8 = id_dict['@i8']
+        else:
+            i8 = bldr.gen_sym('@i8')
+            bldr.new_type_int(i8, 8)
+
+        test_fnc_sig = id_dict['test_fnc_sig']
+        test_fnc = id_dict['test_fnc']
+        result_type = id_dict['result_type']
+
+        if '@i32' in id_dict:
+            i32 = id_dict['@i32']
+        else:
+            i32 = bldr.gen_sym('@i32')
+            bldr.new_type_int(i32, 32)
+        dbl = id_dict['@dbl']
+        pi8 = bldr.gen_sym("@pi8")
+        bldr.new_type_uptr(pi8, i8)
+        ppi8 = bldr.gen_sym("@ppi8")
+        bldr.new_type_uptr(ppi8, pi8)
+        pi = bldr.gen_sym("@pi");
+        bldr.new_const_double(pi, dbl, 3.141593)
+        e = bldr.gen_sym("@e");
+        bldr.new_const_double(e, dbl, 2.71828)
+        result = bldr.gen_sym("@result")
+        bldr.new_global_cell(result, result_type)
+        sig_i32ppi8_ = bldr.gen_sym("@sig_i32ppi8_")
+        bldr.new_funcsig(sig_i32ppi8_, [i32, ppi8], [])
+        entry = bldr.gen_sym("@entry")
+        bldr.new_func(entry, sig_i32ppi8_)
+        # function body
+        v1 = bldr.gen_sym("@entry_v1")
+        blk0 = bldr.gen_sym()
+        argc = bldr.gen_sym()
+        argv = bldr.gen_sym()
+        res = bldr.gen_sym()
+        op_call = bldr.gen_sym()
+        bldr.new_call(op_call, [res], test_fnc_sig, test_fnc, [pi, e])
+        op_store = bldr.gen_sym()
+        bldr.new_store(op_store, False, rmu.MuMemOrd.NOT_ATOMIC, result_type, result, res)
+        op_exit = bldr.gen_sym()
+        bldr.new_comminst(op_exit, [], rmu.MuCommInst.THREAD_EXIT, [], [], [], [])
+        bldr.new_bb(blk0, [argc, argv], [i32, ppi8], rmu.MU_NO_ID, [op_call, op_store, op_exit])
+        bldr.new_func_ver(v1, entry, [blk0])
+
+        id_dict.update({
+            '@entry': entry,
+            '@i32': i32,
+            '@ppi8': ppi8,
+            '@result': result
+        })
+
+    res = impl_jit_test(cmdopt, build_test_bundle, extend_with_entrypoint)
+    if cmdopt.run:
+        assert res == 5.859873
+
+
+def test_double_sitofp(cmdopt):
+    def build_test_bundle(bldr, rmu):
+        """
+        Builds the following test bundle.
+            .typedef @dbl = double
+            .typedef @i1 = int<1>
+            .typedef @i64 = int<64>
+            .const @k <@i64> = -42
+            .funcsig @sig__dbl = () -> (@dbl)
+            .funcdef @test_fnc VERSION @test_fnc.v1 <@sig__dbl> {
+                %blk0():
+                    %res = SITOFP <@i64 @dbl> @k
+                    RET %res
+            }
+        :type bldr: rpython.rlib.rmu.MuIRBuilder
+        :type rmu: rpython.rlib.rmu
+        :return: (rmu.MuVM(), rmu.MuCtx, rmu.MuIRBuilder, MuID, MuID)
+        """
+        dbl = bldr.gen_sym("@dbl"); bldr.new_type_double(dbl)
+        i1 = bldr.gen_sym("@i1"); bldr.new_type_int(i1, 1)
+        i64 = bldr.gen_sym("@i64"); bldr.new_type_int(i64, 64)
+
+        k = bldr.gen_sym("@k"); bldr.new_const_int(k, i64, -42)
+
+        sig__dbl = bldr.gen_sym("@sig__dbl"); bldr.new_funcsig(sig__dbl, [], [dbl])
+
+        test_fnc = bldr.gen_sym("@test_fnc"); bldr.new_func(test_fnc, sig__dbl)
+
+        test_fnc_v1 = bldr.gen_sym("@test_fnc.v1")
+        blk0 = bldr.gen_sym("@test_fnc.v1.blk0")
+        res = bldr.gen_sym("@test_fnc.v1.blk0.res")
+        op_sitofp = bldr.gen_sym(); bldr.new_conv(op_sitofp, res, rmu.MuConvOptr.SITOFP, i64, dbl, k)
+        op_ret = bldr.gen_sym(); bldr.new_ret(op_ret, [res])
+        bldr.new_bb(blk0, [], [], rmu.MU_NO_ID, [op_sitofp, op_ret])
+
+        bldr.new_func_ver(test_fnc_v1, test_fnc, [blk0])
+
+        return {
+            "test_fnc": test_fnc,
+            "test_fnc_sig": sig__dbl,
+            "result_type": dbl,
+            "handle_conv_dst_type": 'double',
         }
     res = impl_jit_test(cmdopt, build_test_bundle)
+    if cmdopt.run:
+        assert res == -42.0
+
+
+def test_double_fptosi(cmdopt):
+    def build_test_bundle(bldr, rmu):
+        """
+        Builds the following test bundle.
+            .typedef @dbl = double
+            .typedef @i1 = int<1>
+            .typedef @i64 = int<64>
+            .const @npi <@i64> = -3.1415926
+            .funcsig @sig__i64 = () -> (@dbl)
+            .funcdef @test_fnc VERSION @test_fnc.v1 <@sig__i64> {
+                %blk0():
+                    %res = FPTOSI <@dbl @i64> @npi
+                    RET %res
+            }
+        :type bldr: rpython.rlib.rmu.MuIRBuilder
+        :type rmu: rpython.rlib.rmu
+        :return: (rmu.MuVM(), rmu.MuCtx, rmu.MuIRBuilder, MuID, MuID)
+        """
+        dbl = bldr.gen_sym("@dbl"); bldr.new_type_double(dbl)
+        i1 = bldr.gen_sym("@i1"); bldr.new_type_int(i1, 1)
+        i64 = bldr.gen_sym("@i64"); bldr.new_type_int(i64, 64)
+
+        npi = bldr.gen_sym("@npi"); bldr.new_const_double(npi, dbl, -3.1415926)
+
+        sig__i64 = bldr.gen_sym("@sig__i64"); bldr.new_funcsig(sig__i64, [], [i64])
+
+        test_fnc = bldr.gen_sym("@test_fnc"); bldr.new_func(test_fnc, sig__i64)
+
+        test_fnc_v1 = bldr.gen_sym("@test_fnc.v1")
+        blk0 = bldr.gen_sym("@test_fnc.v1.blk0")
+        res = bldr.gen_sym("@test_fnc.v1.blk0.res")
+        op_fptosi = bldr.gen_sym(); bldr.new_conv(op_fptosi, res, rmu.MuConvOptr.FPTOSI, dbl, i64, npi)
+        op_ret = bldr.gen_sym(); bldr.new_ret(op_ret, [res])
+        bldr.new_bb(blk0, [], [], rmu.MU_NO_ID, [op_fptosi, op_ret])
+
+        bldr.new_func_ver(test_fnc_v1, test_fnc, [blk0])
+
+        return {
+            "test_fnc": test_fnc,
+            "test_fnc_sig": sig__i64,
+            "result_type": i64,
+            "handle_conv_dst_type": 'sint64',
+        }
+    res = impl_jit_test(cmdopt, build_test_bundle)
+    if cmdopt.run:
+        assert res == -3
+
+
+def test_double_uitofp(cmdopt):
+    def build_test_bundle(bldr, rmu):
+        """
+        Builds the following test bundle.
+            .typedef @dbl = double
+            .typedef @i1 = int<1>
+            .typedef @i64 = int<64>
+            .const @k <@i64> = 42
+            .funcsig @sig__dbl = () -> (@dbl)
+            .funcdef @test_fnc VERSION @test_fnc.v1 <@sig__dbl> {
+                %blk0():
+                    %res = SITOFP <@i64 @dbl> @k
+                    RET %res
+            }
+        :type bldr: rpython.rlib.rmu.MuIRBuilder
+        :type rmu: rpython.rlib.rmu
+        :return: (rmu.MuVM(), rmu.MuCtx, rmu.MuIRBuilder, MuID, MuID)
+        """
+        dbl = bldr.gen_sym("@dbl"); bldr.new_type_double(dbl)
+        i1 = bldr.gen_sym("@i1"); bldr.new_type_int(i1, 1)
+        i64 = bldr.gen_sym("@i64"); bldr.new_type_int(i64, 64)
+
+        k = bldr.gen_sym("@k"); bldr.new_const_int(k, i64, 42)
+
+        sig__dbl = bldr.gen_sym("@sig__dbl"); bldr.new_funcsig(sig__dbl, [], [dbl])
+
+        test_fnc = bldr.gen_sym("@test_fnc"); bldr.new_func(test_fnc, sig__dbl)
+
+        test_fnc_v1 = bldr.gen_sym("@test_fnc.v1")
+        blk0 = bldr.gen_sym("@test_fnc.v1.blk0")
+        res = bldr.gen_sym("@test_fnc.v1.blk0.res")
+        op_sitofp = bldr.gen_sym(); bldr.new_conv(op_sitofp, res, rmu.MuConvOptr.UITOFP, i64, dbl, k)
+        op_ret = bldr.gen_sym(); bldr.new_ret(op_ret, [res])
+        bldr.new_bb(blk0, [], [], rmu.MU_NO_ID, [op_sitofp, op_ret])
+
+        bldr.new_func_ver(test_fnc_v1, test_fnc, [blk0])
+
+        return {
+            "test_fnc": test_fnc,
+            "test_fnc_sig": sig__dbl,
+            "result_type": dbl,
+            "handle_conv_dst_type": 'double',
+        }
+    res = impl_jit_test(cmdopt, build_test_bundle)
+    if cmdopt.run:
+        assert res == 42.0
+
+
+def test_double_fptoui(cmdopt):
+    def build_test_bundle(bldr, rmu):
+        """
+        Builds the following test bundle.
+            .typedef @dbl = double
+            .typedef @i1 = int<1>
+            .typedef @i64 = int<64>
+            .const @pi <@i64> = 3.1415926
+            .funcsig @sig__i64 = () -> (@dbl)
+            .funcdef @test_fnc VERSION @test_fnc.v1 <@sig__i64> {
+                %blk0():
+                    %res = FPTOSI <@dbl @i64> @pi
+                    RET %res
+            }
+        :type bldr: rpython.rlib.rmu.MuIRBuilder
+        :type rmu: rpython.rlib.rmu
+        :return: (rmu.MuVM(), rmu.MuCtx, rmu.MuIRBuilder, MuID, MuID)
+        """
+        dbl = bldr.gen_sym("@dbl"); bldr.new_type_double(dbl)
+        i1 = bldr.gen_sym("@i1"); bldr.new_type_int(i1, 1)
+        i64 = bldr.gen_sym("@i64"); bldr.new_type_int(i64, 64)
+
+        pi = bldr.gen_sym("@pi"); bldr.new_const_double(pi, dbl, 3.1415926)
+
+        sig__i64 = bldr.gen_sym("@sig__i64"); bldr.new_funcsig(sig__i64, [], [i64])
+
+        test_fnc = bldr.gen_sym("@test_fnc"); bldr.new_func(test_fnc, sig__i64)
+
+        test_fnc_v1 = bldr.gen_sym("@test_fnc.v1")
+        blk0 = bldr.gen_sym("@test_fnc.v1.blk0")
+        res = bldr.gen_sym("@test_fnc.v1.blk0.res")
+        op_fptosi = bldr.gen_sym(); bldr.new_conv(op_fptosi, res, rmu.MuConvOptr.FPTOUI, dbl, i64, pi)
+        op_ret = bldr.gen_sym(); bldr.new_ret(op_ret, [res])
+        bldr.new_bb(blk0, [], [], rmu.MU_NO_ID, [op_fptosi, op_ret])
+
+        bldr.new_func_ver(test_fnc_v1, test_fnc, [blk0])
+
+        return {
+            "test_fnc": test_fnc,
+            "test_fnc_sig": sig__i64,
+            "result_type": i64,
+            "handle_conv_dst_type": 'sint64',
+        }
+    res = impl_jit_test(cmdopt, build_test_bundle)
+    if cmdopt.run:
+        assert res == 3
